70ad2d5114ddaea84e1181f3ea87b568eb3a7465
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2023-04-25 13:01:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 25069425
+API_HASH     = "41034e257e6449615faea5f18bbe1dd7"
+BOT_TOKEN    = "6249973506:AAEBOegcZ71m9QlFpNHEvLxl3qFqaX1CuVE"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://Shivam31:Shivam31@cluster0.yrc8krw.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001876507111
+ADMIN        = 6249148586
+CHANNEL      = "@crazybotz"
