d984888583c0459074804a638e51ba89ae42b41d
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2023-05-04 00:42:52 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -7,7 +7,7 @@ async def start(bot, message):
     await add_user(message.from_user.id, message.from_user.first_name)
     await message.reply(text=script.START.format(message.from_user.mention),
                         disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⇆', url=f'http://t.me/Snunfilterbot?startgroup=true')
+                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⇆', url=f'http://t.me/Link_Search_Rbot?startgroup=true')
             ],[InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
                                                             InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")]]))  
 @Client.on_message(filters.command("help"))
@@ -46,16 +46,16 @@ async def misc(bot, update):
     if data=="home":
        await update.message.edit(text=script.START.format(update.from_user.mention),
                                  disable_web_page_preview=True,
-                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⇆', url=f'http://t.me/Snunfilterbot?startgroup=true')
+                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⇆', url=f'http://t.me/Link_Search_Rbot?startgroup=true')
             ],[InlineKeyboardButton("⚡️ ʜᴇʟᴘ", callback_data="misc_help"),
                                                             InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")]])) 
     elif data=="help":
        await update.message.edit(text=script.HELP, 
                                  disable_web_page_preview=True,
-                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⇍  ʙᴀᴄᴋ", callback_data="misc_home")],[InlineKeyboardButton('💌 ᴏᴡɴᴇʀ', url='http://t.me/heartlesssn')]])) 
+                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⇍  ʙᴀᴄᴋ", callback_data="misc_home"),InlineKeyboardButton('ᴏᴡɴᴇʀ', url='http://t.me/heartlesssn')]])) 
 
     elif data=="about":
         await update.message.edit(text=script.ABOUT.format((await bot.get_me()).mention), 
                                   disable_web_page_preview=True,
-                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⇍  ʙᴀᴄᴋ", callback_data="misc_home"),InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ ⇴', url='http://t.me/heartlesssn')]]))
+                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⇍  ʙᴀᴄᴋ", callback_data="misc_home"),InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ ⇴', url='http://t.me/crazysupportz')]]))
          
